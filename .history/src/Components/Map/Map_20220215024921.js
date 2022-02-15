162b2049f4d391eac2aa0c994ef6deec8672af08
Kenya Caldwell <kenya78hami@gmail.com>
2022-02-15 03:29:57 -0600

    displayed weather data and added more comments

---
+++ .history/src/Components/Map/Map_20220215024921.js
@@ -0,0 +1,73 @@
+//component lays out structure for map that displays place and weather data 
+
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import {Rating} from '@mui/material'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import {MAP_STYLES} from '../../mapStyles';
+import useStyles from './styles.js';
+
+
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  console.log(places);
+
+  let keys = [];
+  let data = Number;
+  let icon = '';
+  for (keys in weatherData){
+    console.log(weatherData.weather);
+    console.log(icon);
+  }
+                        
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyA9egc_p2EInLmBckdsnxUhCUOKqkj-orw ' }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true, styles: {MAP_STYLES} }}
+        onChange={(e) => {
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places.length && places.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img alt={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />  
+                </Paper>
+              )}
+          </div>
+        ))}
+         <div>
+           {weatherData.weather.length
+         {/* <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} height="70px" />
+         </div> */}
+         </div>
+
+        <h1>Weather</h1>
+      </GoogleMapReact>
+    </div>
+  );
+};
+export default Map;
